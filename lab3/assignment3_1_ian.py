e8bf3996db0f48b5431d6897c8e6b16a810bf759
ijenkins <ian.jenkins@volvocars.com>
2021-05-03 21:13:33 +0200

    updated 3.1

--- lab3/assignment3_1_ian.py
+++ lab3/assignment3_1_ian.py
@@ -9,16 +9,25 @@ class MRWordFrequencyCount(MRJob):
                    combiner=self.combiner,
                    reducer=self.reducer)]
 
-
     def mapper_get_data(self, _, line): # 2 c -> 2 mappers
-        yield "None", float(line.split()[2])
+        w = line.split('\t')
+        yield "None", float(w[2])
 
-    def combiner(self, key, vaules):
-        for _, j  in enumerate(vaules):
-            yield "Total", (sum(j), 1)
+    def combiner(self, _, values):
+        c = 0
+        for count, _ in enumerate(values):
+            c = count
 
-    def reducer(self, key, values):
-        yield key, sum(values)
+        yield "None", (sum(values), c+1)
+
+    def reducer(self, _, values):
+        c = 0
+        total = 0
+        for count, val in enumerate(values):
+            c = count
+            total=val[0]
+
+        yield "AVG", (total, c+1)
         #yield key, stat.mean(values)
 
 
