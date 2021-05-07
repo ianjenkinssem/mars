69851e5d277b770df6c017ede1fd7c68bebbbb28
Dipti-A <gusaswdi@student.gu.se>
2021-05-08 02:00:25 +0530

    Added changes for histogram data

--- lab3/Problem1a.py
+++ lab3/Problem1a.py
@@ -1,13 +1,12 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
 import math
 
 class CalculateStats(MRJob):
 
     def mapper(self, _, line):
         words = line.split('\t')
-        grp = int(words[1])
-        yield "None", float(words[2])  # Yield <value>
+        # Yield <value> column
+        yield "None", float(words[2])
 
     def combiner(self, _, value):
         count = 0
@@ -19,10 +18,20 @@ class CalculateStats(MRJob):
             count += 1
             values += v
             sumofsquares += v**2
-            listofvalues.append(v)  # Group all the supplied values in a list
+            listofvalues.append(v)
 
-        # Yield (sum of values, count, sumofsquares, min, max) tuple for each mapper
-        yield "None", (values,count,sumofsquares,min(listofvalues),max(listofvalues))
+        # Yield (sum of values, count, sum of squares, min, max, list of values) tuple for each mapper
+        yield "None", (values, count, sumofsquares, min(listofvalues), max(listofvalues), listofvalues)
+
+    # Function to calculate frequency of values in given range
+    def count_records_in_range(self, values, minrange, maxrange):
+        counter = 0
+        # Loop thru all values
+        for val in values:
+            if val >= minrange and val <= maxrange:
+                counter += 1
+        return counter
+
 
     def reducer(self, _, tuple):
         counts = 0
@@ -30,6 +39,7 @@ class CalculateStats(MRJob):
         sumofsquares = 0
         listofminvalues = []
         listofmaxvalues = []
+        listofvalues = []
 
         for idx,tup in enumerate(tuple): # Iterating through Iterator object
             values += tup[0]  # Calculate sum of values
@@ -37,23 +47,51 @@ class CalculateStats(MRJob):
             sumofsquares += tup[2] # Aggregate total sum of squares of all values
             listofminvalues.append(tup[3])  # Group all minimum values from each combiner
             listofmaxvalues.append(tup[4])  # Group all maximum values from each combiner
+            listofvalues.append(tup[5])  # Group all the values from each combiner
+
+        # Retrieve min and max values from list retrieved from combiner
+        minval = min(listofminvalues)
+        maxval = max(listofmaxvalues)
 
         # Calculate Mean
-        mean = values/counts
+        mean = values / counts
 
-        # Calculate std deviation
-        stddev = math.sqrt(sumofsquares/counts - mean**2)
+        # Calculate Std. deviation
+        stddev = math.sqrt(sumofsquares / counts - mean ** 2)
 
-        # Yield Mean, Std deviation,Min, Max values
+        # Yield Mean, Std. deviation, Min, Max values
         yield "Mean", mean
-        yield "Stddev",stddev
-        yield "Min value", min(listofminvalues)
-        yield "Max value", max(listofmaxvalues)
+        yield "Stddev", stddev
+        yield "MinValue", minval
+        yield "MaxValue", maxval
 
-    def steps(self):
-        return [MRStep(mapper=self.mapper,
-                       combiner=self.combiner,
-                       reducer=self.reducer)]
+        ### Calculate data for histogram - Bin interval and the number of records in that range ###
+        numofbins = 10
+        listofallvalues = []
 
-#if __name__ == '__main__':
-    #CalculateStats.run()
+        # Read all the lists of values from combiner and convert them into a single flat list
+        for elem in listofvalues:
+            listofallvalues.extend(elem)
+
+        # Calculate bin width - << Not rounding off the bin width as the values are of float type>>
+        bin_width = (maxval - minval)/numofbins
+
+        # Create list of bin intervals with minimum value
+        bin_intervals = [minval]
+        newval = minval
+
+        # Calculate bin-intervals
+        for i in range(numofbins):
+            # bin interval = previous value + bin width
+            newval += bin_width
+            if newval < (maxval + bin_width):
+                bin_intervals.append((newval))
+
+        # Calculate bin-intervals and frequency of values in that range and yield results
+        # <<Not rounding off the bin-intervals as they are long float values>>
+        for i in range(len(bin_intervals) - 1):
+            yield (bin_intervals[i], bin_intervals[i + 1]), self.count_records_in_range(listofallvalues, bin_intervals[i],bin_intervals[i + 1])
+
+
+if __name__ == '__main__':
+    CalculateStats.run()
